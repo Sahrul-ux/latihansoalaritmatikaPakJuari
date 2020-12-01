a3e01ee274e459945b3488b1f20d456ff612fe3d
Sahrul-ux <sahrulrajab11@gmail.com>
2020-12-01 10:05:55 +0700

    latihan soal aritmatika pak juari

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      int a = 6;
+      int b = 8;
+      int c = 5;
+
+     int T;
+
+    //penjumlahan
+    cout << "Nama : Sahrul Rojab \n";
+    cout << "Nim : 1855201092 \n";
+    cout << "Prodi : Teknik Informatika \n";
+    cout << "========================== \n \n";
+    
+    //rumus
+    T = a + b - c;
+    cout << a << " + " << b << " - " << c << " = " << T <<endl;
+
+
+
+    cin.get();
+    return 0;
+}
+
